7a0672d27cce8770d31f5cafe171205f2111c395
Bert Zahniser <bzahniser@localhost.localdomain>
2017-01-24 18:25:51 -0500

    fixed case on Framework/framework

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/./framework/yii.php';
-//$yii=dirname(__FILE__).'/./framework/yii.php';
+$yii=dirname(__FILE__).'/./Framework/yii.php';
+//$yii=dirname(__FILE__).'/./Framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
